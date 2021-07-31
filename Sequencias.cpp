658147de5f126352a1362052cd0a0d20a0baa986
PedrixPH <pedro.h.gomes.2002@gmail.com>
2021-07-31 16:02:33 -0400

    update

--- Sequencias.cpp
+++ Sequencias.cpp
@@ -1,22 +1,19 @@
 #include <stdio.h>
 int a, b;
 int cont = 0;
-int cont_2 = 0;
 
 int main(){
 	scanf("%d%d", &a, &b);
 	for(int num = 1; num <= b; num++){
 		cont++;
-		cont_2++;
 		printf("%d", num);
-		if (cont_2 = num){
-			printf(" ");
-		}
 		if (cont == a){
-		printf("\n");}
-			if (cont == a){
-				cont = 0;
-			}
+			printf("\n");
+			cont = 0;
+		}
+		else{
+			printf(" ");
+		}	
 	}
 	return 0;
 }
